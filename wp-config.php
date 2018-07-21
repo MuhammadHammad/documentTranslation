de09381c9c046f554d90f877794da52a3fbeb7de
Muhammad Hammad Khan <hammadtahirkhan@ymail.com>
2018-07-21 23:15:41 +0200

    corrected db name

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'superadmin');
+define('DB_NAME', 'documenttranslate');
 
 /** MySQL database username */
 define('DB_USER', 'superAdmin');
